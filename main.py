f5115c2942660307ac459b18c13a6d29db4509df
CyberPunk58 <pavelrip@mail.ru>
2023-04-21 17:26:25 +0300

    sum row added

--- main.py
+++ main.py
@@ -6,10 +6,17 @@ df1 = pd.DataFrame()
 
 fileCSV = os.listdir("D:\\Pavel\\Data\\nstats_032023")
 filesCSV = [os.path.join("D:\\Pavel\\Data\\nstats_032023", file) for file in fileCSV]
-filesCSVsorted = sorted(filesCSV, key = os.path.getmtime)
+filesCSVsorted = sorted(filesCSV, key = os.path.getmtime) #Сортируем по дате создания файла для нужного порядка в итоговом файле
 for file in range(0, len(filesCSVsorted)):
     df2 = pd.read_csv(filesCSVsorted[file], sep=',')
     df2 = df2.drop(columns='day')
     df1 = pd.concat((df1,df2),axis = 1)
 
-df1.to_excel("D:\\Pavel\\Data\\nstats_032023\\"+"itog"+".xlsx", index=False)
+df1.to_excel("D:\\Pavel\\Data\\nstats_032023_total\\"+"total"+".xlsx", index=False)
+
+
+df = pd.read_excel("D:\\Pavel\\Data\\nstats_032023_total\\total.xlsx")
+df['row_sum'] = df.sum (axis=1)
+df.to_excel("D:\\Pavel\\Data\\nstats_032023_total\\"+"total_with_sum"+".xlsx", index=False)
+
+
